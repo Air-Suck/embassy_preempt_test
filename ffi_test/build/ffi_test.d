433de23e5247197582433b2caa2e17b3829e53a0
snh <2640327187@qq.com>
2024-09-23 11:45:48 +0800

    add ffi test

---
+++ ffi_test/build/ffi_test.d
@@ -0,0 +1,10 @@
+build/ffi_test.o: src/ffi_test.c inc/stm32f401xe.h inc/core_cm4.h \
+ inc/cmsis_version.h inc/cmsis_compiler.h inc/cmsis_gcc.h inc/mpu_armv7.h \
+ inc/system_stm32f4xx.h
+inc/stm32f401xe.h:
+inc/core_cm4.h:
+inc/cmsis_version.h:
+inc/cmsis_compiler.h:
+inc/cmsis_gcc.h:
+inc/mpu_armv7.h:
+inc/system_stm32f4xx.h:
